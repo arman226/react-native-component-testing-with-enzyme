510e5492d56104bdc186c93819882454ac5a4148
Armando Bibay <armandobibayjr@Armandos-MacBook-Pro.local>
2021-07-14 16:44:16 +0800

    Initiate component testing

--- SampleComponent.js
+++ SampleComponent.js
@@ -9,9 +9,9 @@ const SampleComponent = () => {
   };
 
   return (
-    <View>
-      <Text>{isToggled ? 'I am active' : 'I am inactive'}</Text>
-      <Button title="Click Me" onPress={handleClick} />
+    <View testid="sample">
+      <Text testid="text0">{isToggled ? 'I am active' : 'I am inactive'}</Text>
+      <Button testid="button" title="Click Me" onPress={handleClick} />
     </View>
   );
 };
--- SampleComponent.test.js
+++ SampleComponent.test.js
@@ -1,5 +1,24 @@
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import SampleComponent from './SampleComponent';
+import {expect} from '@jest/globals';
+
+Enzyme.configure({adapter: new Adapter()});
+
 describe('SampleComponent', () => {
   it('should run properly', () => {
-    expect(true).toBe(true);
+    const wrapper = shallow(<SampleComponent />);
+    //trigger button press
+    wrapper
+      .findWhere(node => node.prop('testid') === 'button')
+      .simulate('press');
+
+    //check if the text has changed after pressing the button
+    const text = wrapper
+      .findWhere(node => node.prop('testid') === 'text0')
+      .render();
+
+    expect(text.text()).toBe('I am active');
   });
 });
