eadee13f7e6d8a0f6e5248fa31caf5b22ef45bb9
Armando Bibay <armandobibayjr@Armandos-MacBook-Pro.local>
2021-07-15 15:19:56 +0800

    refactor

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # react-native-component-testing-with-enzyme
 
-
 ## DEPENDENCIES
 
-1. ***enzyme*** `npm install --save-dev enzyme enzyme-adapter-react-16`
+1. **_enzyme_** `npm install --save-dev enzyme enzyme-adapter-react-16`
+
+## RUNNING TEST
+
+`npm run test`
--- SampleComponent.test.js
+++ SampleComponent.test.js
@@ -1,24 +1,34 @@
 import React from 'react';
 import Enzyme, {shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import SampleComponent from './SampleComponent';
 import {expect} from '@jest/globals';
+import SampleComponent from './SampleComponent';
 
 Enzyme.configure({adapter: new Adapter()});
 
-describe('SampleComponent', () => {
-  it('should run properly', () => {
+describe('Sample Component', () => {
+  it('should Display "I am active" after  the button press', () => {
+    const TEXT_TEST_ID = 'text';
+    const BUTTON_TEST_ID = 'button';
     const wrapper = shallow(<SampleComponent />);
+
     //trigger button press
-    wrapper
-      .findWhere(node => node.prop('testid') === 'button')
-      .simulate('press');
+    const button = wrapper.find({testid: BUTTON_TEST_ID});
+
+    //initial press
+    button.simulate('press');
 
     //check if the text has changed after pressing the button
-    const text = wrapper
-      .findWhere(node => node.prop('testid') === 'text')
-      .render();
+    const text = wrapper.find({testid: TEXT_TEST_ID}).dive();
 
     expect(text.text()).toBe('I am active');
+
+    // press the button 2nd time
+    button.simulate('press');
+
+    // new instance of the textview to change whether its value has changed after the press event
+    const textChanged = wrapper.find({testid: TEXT_TEST_ID}).dive();
+
+    expect(textChanged.text()).toBe('I am inactive');
   });
 });
